064212227e88bea01004895051fb6460d3939a2d
abzom <129222953+abzom@users.noreply.github.com>
2024-02-29 10:27:39 +0000

    Delete c++program directory

--- c++program/fistlab.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main(){
-
-int dayweek = 7;
-cout << dayweek;
-
-
-}
--- c++program/lab1/lab1.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main()
-{
-  cout << "\n Thanks for viewing my code!";
-  return 0;
-}
